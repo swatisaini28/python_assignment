55fb4aadca98f0c571c7164f204a4c16dfa6084c
swatisaini28 <sainiswati2827@gmail.com>
2025-05-27 22:13:42 +0530

    ASSIGNMENT_1_python

---
+++ assignment 1 problem 1.py
@@ -0,0 +1,12 @@
+a=float(input("Enter the number:"))
+b=float(input("Enter the number:"))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
+
+
---
+++ assignment 1 problem 2.py
@@ -0,0 +1,4 @@
+First_name=input("Enter your first name:")
+Last_name=input("Enter your last name:")
+Full_name =First_name +" " +Last_name
+print("Hello "+ Full_name+" ! Welcome to python program")
